0c013838b6e5103b287646eda42a27717545779c
lkara001 <lkara001@gold.ac.uk>
2019-04-25 14:36:11 +0100

    Initial Commit

---
+++ readFromCSV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  readFromCSV
+//
+//  Created by Lydia Kara on 25/04/2019.
+//  Copyright © 2019 Lydia Kara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
